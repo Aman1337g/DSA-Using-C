c4bd13af2a101ce158a965a9c76ead30cda32a00
Aman Kumar Gupta <aman65144kumar@gmail.com>
2022-08-17 19:37:04 +0530

    BT Traversal - Inorder, Preorder, Postorder Added

---
+++ 05Trees/05BT_Traversals(Inorder, Preorder and Postorder).c
@@ -0,0 +1,20 @@
+/*                          Binary Tree Traversals
+
+  1. Inorder Traversal - Left Root Right    [Root in between Left and Right]
+  2. Preorder Traversal - Root Left Right
+  3. Postorder Traversal - Left Right Root
+
+  Consider a binary tree - 
+
+                   A
+                /     \
+               B       C
+                \     / \
+                 D   E   F
+                    /   / \
+                   G   H   I  
+
+   1. Inorder - BDAGECHFI
+   2. Preorder - ABDCEGFHI
+   3. Postorder - DBGEHIFCA
+*/
